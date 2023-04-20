35a3679384f0dd94d368e850f8657a6bfe932f82
Rustam <rust17@yandex.ru>
2023-04-20 23:00:13 +0300

    early_access

--- sochibash/sochibash/urls.py
+++ sochibash/sochibash/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('schedule/', include('schedule.urls', namespace='schedule')),
     path('shop/', include('etnoshop.urls', namespace='etnoshop')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT

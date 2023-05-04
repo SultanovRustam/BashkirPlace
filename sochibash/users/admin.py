79a86a250c0aff8a0df1e499795da4c1881aa4f0
Rustam <rust17@yandex.ru>
2023-05-04 11:28:43 +0300

    v 1.001

--- sochibash/users/admin.py
+++ sochibash/users/admin.py
@@ -7,11 +7,10 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
-        'username',
         'first_name',
         'last_name',
         'email',
     )
     ordering = ('email',)
-    search_fields = ('username', 'email', 'last_name')
-    list_filter = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('email', 'last_name')
+    list_filter = ('email', 'first_name', 'last_name')

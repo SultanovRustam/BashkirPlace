733ae90c8870722bb48b1cd94524b69f54dd4090
Rustam <rust17@yandex.ru>
2023-05-27 11:04:43 +0300

    v 1.03 update admin panel

--- sochibash/events/admin.py
+++ sochibash/events/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Event, Gallery
 
@@ -10,4 +11,17 @@ class GalleryInline(admin.TabularInline):
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    list_display = ["preview", "title", "author", "description", "event_date"]
     inlines = [GalleryInline, ]
+
+    def preview(self, obj):
+        images = obj.images.all()[:3]
+
+        if images:
+            return format_html(
+                ''.join(['<img src="{}" width="200" height="250"/>'.format(
+                    image.image.url) for image in images]))
+        else:
+            return '-'
+
+    preview.short_description = 'Превью изображений'
--- sochibash/news/admin.py
+++ sochibash/news/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Gallery, News
 
 
-class GalleryInline(admin.TabularInline):
+class GalleryInline(admin.StackedInline):
     fk_name = 'news'
     model = Gallery
 
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
+    list_display = ["preview", "title", "author", "text"]
     inlines = [GalleryInline, ]
+
+    def preview(self, obj):
+        images = obj.images.all()[:3]
+
+        if images:
+            return format_html(
+                ''.join(['<img src="{}" width="200" height="250"/>'.format(
+                    image.image.url) for image in images]))
+        else:
+            return '-'
+
+    preview.short_description = 'Превью изображений'
